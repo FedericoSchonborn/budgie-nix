78f9ccf97c7158222c2749171e5b1bbcf19726a1
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-08-31 00:17:41 -0300

    vm: Use Slick Greeter
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- systems/vm/default.nix
+++ systems/vm/default.nix
@@ -6,7 +6,7 @@
   users.users.vm.isNormalUser = true;
   services.xserver.enable = true;
   services.xserver.desktopManager.budgie.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.greeters.slick.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "vm";
   system.stateVersion = "22.11";
