489862fba82513d02bef28d8a826ff2ba74680c5
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-08-31 01:10:07 -0300

    Work on the NixOS module
    - Add budgie-backgrounds package.
    - Add xdg-user-dirs for creating user directories.
    - Add networkmanagerapplet if NetworkManager is enabled.
    - Enable dconf and dbus for packages that require it.
    - Enable Polkit (for budgie-polkit).
    - Set Bluetooth, colord and NetworkManager to be enabled by default (those
    can be overriden in the system configuration).
    - Add PAM service for Budgie Screensaver.
    
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- modules/budgie/default.nix
+++ modules/budgie/default.nix
@@ -13,18 +13,48 @@ in
   config = mkMerge [
     (mkIf cfg.enable {
       services.xserver.displayManager.sessionPackages = with pkgs; [ budgie-desktop ];
-      environment.systemPackages = with pkgs;[
+
+      environment.systemPackages = with pkgs; [
+        # Budgie Desktop.
+        budgie-backgrounds
         budgie-desktop
         budgie-desktop-view
         budgie-control-center
         budgie-screensaver
 
-        # Required by the Budgie Desktop session
+        # Create user directories.
+        xdg-user-dirs
+
+        # Required by the Budgie Desktop session.
         gnome.gnome-session
 
-        # Required by Budgie Menu
+        # Required by Budgie Menu.
         gnome-menus
+      ] ++ (
+        # Network Manager applet.
+        optional config.networking.networkmanager.enable networkmanagerapplet
+      );
+
+      # Required by backgrounds.
+      environment.pathsToLink = [
+        "/share"
       ];
+
+      # Required by the Budgie Desktop session.
+      programs.dconf.enable = true;
+
+      # Required by Budgie Control Center.
+      hardware.bluetooth.enable = mkDefault true;
+      networking.networkmanager.enable = mkDefault true;
+      services.colord.enable = mkDefault true;
+      services.dbus.enable = true;
+      services.dbus.packages = with pkgs; [ budgie-control-center ];
+
+      # Required by Budgie Desktop.
+      security.polkit.enable = true;
+
+      # Required by Budgie Screensaver.
+      security.pam.services.budgie-screensaver = { };
     })
   ];
 }
