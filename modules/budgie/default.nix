74ff2d29fb38d09149ec283020243541a1d074dc
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-09-01 22:22:35 -0300

    Remove mkMerge
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- modules/budgie/default.nix
+++ modules/budgie/default.nix
@@ -19,54 +19,52 @@ in {
     };
   };
 
-  config = mkMerge [
-    (mkIf cfg.enable {
-      services.xserver.displayManager.sessionPackages = with pkgs; [budgie.budgie-desktop];
+  config = mkIf cfg.enable {
+    services.xserver.displayManager.sessionPackages = with pkgs; [budgie.budgie-desktop];
 
-      environment.systemPackages = with pkgs;
-        [
-          # Budgie Desktop.
-          budgie.budgie-backgrounds
-          budgie.budgie-desktop
-          budgie.budgie-desktop-view
-          budgie.budgie-control-center
-          budgie.budgie-screensaver
+    environment.systemPackages = with pkgs;
+      [
+        # Budgie Desktop.
+        budgie.budgie-backgrounds
+        budgie.budgie-desktop
+        budgie.budgie-desktop-view
+        budgie.budgie-control-center
+        budgie.budgie-screensaver
 
-          # Create user directories.
-          xdg-user-dirs
+        # Create user directories.
+        xdg-user-dirs
 
-          # Required by the Budgie Desktop session.
-          gnome.gnome-session
+        # Required by the Budgie Desktop session.
+        gnome.gnome-session
 
-          # Required by Budgie Menu.
-          gnome-menus
-        ]
-        ++ cfg.appletPackages
-        ++ (
-          # Network Manager applet.
-          optional config.networking.networkmanager.enable networkmanagerapplet
-        );
+        # Required by Budgie Menu.
+        gnome-menus
+      ]
+      ++ cfg.appletPackages
+      ++ (
+        # Network Manager applet.
+        optional config.networking.networkmanager.enable networkmanagerapplet
+      );
 
-      # Required by backgrounds.
-      environment.pathsToLink = [
-        "/share"
-      ];
+    # Required by backgrounds.
+    environment.pathsToLink = [
+      "/share"
+    ];
 
-      # Required by the Budgie Desktop session.
-      programs.dconf.enable = true;
+    # Required by the Budgie Desktop session.
+    programs.dconf.enable = true;
 
-      # Required by Budgie Control Center.
-      hardware.bluetooth.enable = mkDefault true;
-      networking.networkmanager.enable = mkDefault true;
-      services.colord.enable = mkDefault true;
-      services.dbus.enable = true;
-      services.dbus.packages = with pkgs; [budgie.budgie-control-center];
+    # Required by Budgie Control Center.
+    hardware.bluetooth.enable = mkDefault true;
+    networking.networkmanager.enable = mkDefault true;
+    services.colord.enable = mkDefault true;
+    services.dbus.enable = true;
+    services.dbus.packages = with pkgs; [budgie.budgie-control-center];
 
-      # Required by Budgie Desktop.
-      security.polkit.enable = true;
+    # Required by Budgie Desktop.
+    security.polkit.enable = true;
 
-      # Required by Budgie Screensaver.
-      security.pam.services.budgie-screensaver = {};
-    })
-  ];
+    # Required by Budgie Screensaver.
+    security.pam.services.budgie-screensaver = {};
+  };
 }
